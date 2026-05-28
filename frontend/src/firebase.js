5291befb3b2b690ea86d93f2ea68fcc8f038abbb
ADiTyaRaj8969 <adivid198986@gmail.com>
2026-05-28 15:33:13 +0530

    feat: add real Firebase config and update deploy workflow

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,22 +1,18 @@
 import { initializeApp }  from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from 'firebase/auth';
 
-/*
-  FILL IN YOUR FIREBASE CONFIG
-  Go to: https://console.firebase.google.com
-  Create project -> Add Web App -> Copy the config object here
-*/
 const firebaseConfig = {
-  apiKey:            'YOUR_API_KEY',
-  authDomain:        'YOUR_PROJECT_ID.firebaseapp.com',
-  projectId:         'YOUR_PROJECT_ID',
-  storageBucket:     'YOUR_PROJECT_ID.appspot.com',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId:             'YOUR_APP_ID',
+  apiKey:            "AIzaSyCF9t3xPf_Se4qkqAHFRoW-YqG8LfoQIpo",
+  authDomain:        "deepface-aditya.firebaseapp.com",
+  projectId:         "deepface-aditya",
+  storageBucket:     "deepface-aditya.firebasestorage.app",
+  messagingSenderId: "596616284131",
+  appId:             "1:596616284131:web:8ad02bce5645a03e1b42d6",
+  measurementId:     "G-44EVTLV0VL",
 };
 
-const fbApp  = initializeApp(firebaseConfig);
-const auth   = getAuth(fbApp);
+const fbApp          = initializeApp(firebaseConfig);
+const auth           = getAuth(fbApp);
 const googleProvider = new GoogleAuthProvider();
 
 export { auth, googleProvider, signInWithPopup, signOut, onAuthStateChanged };
